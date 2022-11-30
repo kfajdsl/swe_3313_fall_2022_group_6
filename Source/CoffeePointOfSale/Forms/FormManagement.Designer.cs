e9dc4866dd930b5f5d4fc486b062fb7c2aaf26de
Sahan Reddy <sahan.reddy.58@gmail.com>
2022-11-30 14:04:17 -0500

    Add CSV generation

--- Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormManagement.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.GenReportBtn = new System.Windows.Forms.Button();
             this.OrderDrinkTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnClose
             // 
             this.btnClose.BackColor = System.Drawing.Color.Firebrick;
-            this.btnClose.Font = new System.Drawing.Font("Lato", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnClose.ForeColor = System.Drawing.Color.White;
             this.btnClose.Location = new System.Drawing.Point(12, 11);
             this.btnClose.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -47,27 +47,28 @@
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.OnClickBtnClose);
             // 
-            // button2
+            // GenReportBtn
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
-            this.button2.Font = new System.Drawing.Font("Lato", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(459, 254);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(397, 158);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Generate Report";
-            this.button2.UseVisualStyleBackColor = false;
+            this.GenReportBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
+            this.GenReportBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.GenReportBtn.ForeColor = System.Drawing.Color.White;
+            this.GenReportBtn.Location = new System.Drawing.Point(459, 254);
+            this.GenReportBtn.Name = "GenReportBtn";
+            this.GenReportBtn.Size = new System.Drawing.Size(397, 158);
+            this.GenReportBtn.TabIndex = 2;
+            this.GenReportBtn.Text = "Generate Report";
+            this.GenReportBtn.UseVisualStyleBackColor = false;
+            this.GenReportBtn.Click += new System.EventHandler(this.GenReportBtn_Click);
             // 
             // OrderDrinkTitle
             // 
             this.OrderDrinkTitle.AutoSize = true;
-            this.OrderDrinkTitle.Font = new System.Drawing.Font("Lato", 63.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.OrderDrinkTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 63.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.OrderDrinkTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(82)))), ((int)(((byte)(94)))));
             this.OrderDrinkTitle.Location = new System.Drawing.Point(397, 11);
             this.OrderDrinkTitle.Name = "OrderDrinkTitle";
             this.OrderDrinkTitle.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.OrderDrinkTitle.Size = new System.Drawing.Size(540, 102);
+            this.OrderDrinkTitle.Size = new System.Drawing.Size(545, 96);
             this.OrderDrinkTitle.TabIndex = 4;
             this.OrderDrinkTitle.Text = "Management";
             this.OrderDrinkTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -79,7 +80,7 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1332, 682);
             this.Controls.Add(this.OrderDrinkTitle);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.GenReportBtn);
             this.Controls.Add(this.btnClose);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormManagement";
@@ -93,7 +94,7 @@
         #endregion
 
         private Button btnClose;
-        private Button button2;
+        private Button GenReportBtn;
         private Label OrderDrinkTitle;
     }
 }
