ecfce9b591b3cedfc2f5a8c145ea7d4b9303e32c
Sahan Reddy <sahan.reddy.58@gmail.com>
2022-11-29 23:52:47 -0500

    New drinkmenu json

--- Source/CoffeePointOfSale/Services/DrinkMenu/DrinkMenuDrink.cs
+++ Source/CoffeePointOfSale/Services/DrinkMenu/DrinkMenuDrink.cs
@@ -6,7 +6,6 @@ public class DrinkMenuDrink
 {
     public string Name { get; set; } = "";
     public string BaseDescription { get; set; } = "";
-    public string Image { get; set; } = "";
     public decimal BasePrice { get; set; } = 0.0M;
 
     public Drink NewDrink()
