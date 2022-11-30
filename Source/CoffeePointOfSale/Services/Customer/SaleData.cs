e3a33129eb010fa8f63ce00f498d4f9a3fc4ec80
Sahan Reddy <sahan.reddy.58@gmail.com>
2022-11-30 13:59:31 -0500

    add saledata

---
+++ Source/CoffeePointOfSale/Services/Customer/SaleData.cs
@@ -0,0 +1,13 @@
+﻿namespace CoffeePointOfSale.Services.Customer;
+
+public class SaleData
+{
+    public string CustomerId { get; set; } = "";
+    public DateTime TransactionDateTime { get; set; } = DateTime.MinValue;
+    public decimal SubTotal { get; set; } = 0.0M;
+    public decimal Tax { get; set; } = 0.0M;
+    public decimal Total { get; set; } = 0.0M;
+    public string Payment { get; set; } = "";
+    public int RewardsPointsRedeemed { get; set; } = 0;
+    public string OrderDescription { get; set; } = "";
+}
