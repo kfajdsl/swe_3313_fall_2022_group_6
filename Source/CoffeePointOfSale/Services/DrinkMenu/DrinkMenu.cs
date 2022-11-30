234918fbb71141382d1db3779a101ed6d184ec18
Sahan Reddy <sahan.reddy.58@gmail.com>
2022-11-30 12:23:17 -0500

    Add seperate sizes

--- Source/CoffeePointOfSale/Services/DrinkMenu/DrinkMenu.cs
+++ Source/CoffeePointOfSale/Services/DrinkMenu/DrinkMenu.cs
@@ -17,14 +17,26 @@ public class DrinkMenu
     private readonly List<DrinkMenuCustomization> _customizationList = new();
 
     /// <summary>
+    /// Private list containing all customizations.
+    /// </summary>
+    [JsonProperty("Sizes")]
+    private readonly List<DrinkMenuCustomization> _sizeList = new();
+
+    /// <summary>
     /// Returns a readonly list of all drinks.
     /// </summary>
     [JsonIgnore] //not written to JSON file... this is a list view of the dictionary, so only the dictionary is written
     public IReadOnlyList<DrinkMenuDrink> DrinkList => _drinkList;
 
     /// <summary>
-    /// Returns a readonly list of all customizations.
+    /// Returns a readonly list of all customizations (except sizes, which are mutually exclusive).
     /// </summary>
     [JsonIgnore] //not written to JSON file... this is a list view of the dictionary, so only the dictionary is written
     public IReadOnlyList<DrinkMenuCustomization> CustomizationList => _customizationList;
+
+    /// <summary>
+    /// Returns a readonly list of all sizes.
+    /// </summary>
+    [JsonIgnore] //not written to JSON file... this is a list view of the dictionary, so only the dictionary is written
+    public IReadOnlyList<DrinkMenuCustomization> SizeList => _sizeList;
 }
