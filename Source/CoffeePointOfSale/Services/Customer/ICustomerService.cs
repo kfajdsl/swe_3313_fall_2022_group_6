890d0ad6a45a853c1b4ee3bc77dd18997820b3ec
Sahan Reddy <sahan.reddy.58@gmail.com>
2022-11-30 01:16:28 -0500

    Update ICustomerService

--- Source/CoffeePointOfSale/Services/Customer/ICustomerService.cs
+++ Source/CoffeePointOfSale/Services/Customer/ICustomerService.cs
@@ -3,5 +3,12 @@
 public interface ICustomerService
 {
     Customers Customers { get; init; }
+
+    // MUST BE SET PROPERLY BEFORE USE
+    Customer CurrentCustomer { get; set; }
+
+    // MUST BE SET PROPERLY BEFORE USE
+    Order CurrentOrder { get; set; }
+
     void Write();
 }
